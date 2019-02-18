c7f5436ae2d1590c61d6751d7dcb62cb5252044a
Rob Dana <robdana@rdtechsolutions.com>
2019-02-18 08:07:33 -0500

    gift page refactor

--- resources/views/layouts/standard/partials/header_transparent.blade.php
+++ resources/views/layouts/standard/partials/header_transparent.blade.php
@@ -1,12 +1,12 @@
 <header @if(isset($page)) id = "gift_head" data-page-id="{{$page->id}}" @else id="head" @endif>
     <div class="container-fluid" style = "height: 90px;">
         <div class="row">
-            <div class="fheader col-md-8">
+            <div class="col-sm-8">
                 <a class="navbar-brand" href="/">
                     <img src="/front/img/BirdLogo.png" alt="Fynches" title="" id="fyn_logo_1">
                 </a>
             </div>
-            <div class="fheader col-md-4" id="list">
+            <div class="col-sm-4 text-right" id="list">
                 <div id="div_top_hypers">
                     <ul id="ul_top_hypers">
                         <li><a href="" class="a_top_hypers"> HELP</a></li>
